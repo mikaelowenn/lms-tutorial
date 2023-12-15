1f7ffecd308a5657507f468c96e749049620f689
mikael owen <mikaelowen2003@gmail.com>
2023-12-15 09:13:34 +0800

    Develop sidebar and navbar

---
+++ app/(dashboard)/(route)/teacher/courses/page.tsx
@@ -0,0 +1,9 @@
+const TeacherPage = () => {
+    return ( 
+        <div>
+            Courses Page
+        </div>
+     );
+}
+ 
+export default TeacherPage;
--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -32,7 +32,6 @@ export const SidebarItem = ({
             onClick={onClick}
             type="button"
             className={cn("flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20", isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700")}
-
         >
             <div className="flex items-center gap-x-2 py-4">
                 <Icon
@@ -41,6 +40,8 @@ export const SidebarItem = ({
                 />
                 {label}
             </div>
+            <div className={cn("ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",isActive && "opacity-100")}
+            />
         </button>
     )
 }
