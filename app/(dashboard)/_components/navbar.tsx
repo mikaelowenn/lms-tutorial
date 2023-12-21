8b3fef801c705034202fe5c5f964f171bc903be0
mikael owen <mikaelowen2003@gmail.com>
2023-12-21 11:55:19 +0800

    Added teacher mode

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar =() => {
     return(
-        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+        <div className="p-4 border-w h-full flex items-center bg-white shadow-sm sticky top-0">
             <MobileSidebar/>
             <NavbarRoutes/> 
         </div>
